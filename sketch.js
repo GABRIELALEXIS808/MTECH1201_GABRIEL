007cf28cf067e8629e1fb238518249106d5f9093
TayDoug906 <jadencollado23@gmail.com>
2026-09-14 16:30:59 -0400

    First class

---
+++ sketch.js
@@ -0,0 +1,20 @@
+//Gabriel Alexis canvas
+function setup(){
+  //create canvas()--two arugments (width, height)
+  createCanvas(2000,2000);
+
+
+}
+function draw(){
+  //background-setting our background color
+  background(300);
+
+  //stroke weight--thickness
+  strokeWeight(20);
+
+  //line - two sets of coordinates 
+  line(50,50,100,200);
+
+  line(300,10,40,100);
+}
+
